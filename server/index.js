2625c81823de7b570c88a69c757dec037e36709b
Ori Gold <orig@ottopia.tech>
2020-02-07 23:12:58 +0200

    changed port to caps

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const configs = require('../configs.json');
 //endregion
 
 function main() {
-    const PORT = process.env.port || configs.server.port;
+    const PORT = process.env.PORT || configs.server.port;
     const app = express();
     app.use(bodyParser.json());
     app.use(express.static(path.join(__dirname, 'dist')));
